3f67d31242ad06df121557a450962f6909957fd6
Caleb Nanigah <calebna4@gmail.com>
2024-01-31 18:03:18 +0000

    formats a file

--- cypress/support/page_objects/viamo_apps/advanced_reports/interaction_report/interaction_report_objects.js
+++ cypress/support/page_objects/viamo_apps/advanced_reports/interaction_report/interaction_report_objects.js
@@ -1,32 +1,28 @@
-import AdvancedReports from "../AdvancedReports"
+import AdvancedReports from "../AdvancedReports";
 
 class InteractionReport_Objects extends AdvancedReports {
+  constructor(reportName) {
+    super(reportName);
+  }
 
-    constructor(reportName){
-        super(reportName)
+  addFilters(nameTree, columnType = "Tree Results") {
+    // opens the 'Add filer' modal
+    cy.get(".content > .text-right > :nth-child(2)").click();
+
+    if (columnType == "Tree Results") {
+      cy.contains("a", columnType).click();
+      cy.get(".multiselect__tags").click();
+      cy.get(".multiselect__tags").type(`${nameTree} {enter}`);
     }
 
-    addFilters(nameTree, columnType = "Tree Results"){
-
-        // opens the 'Add filer' modal
-        cy.get('.content > .text-right > :nth-child(2)').click()
-        
-        if(columnType == "Tree Results"){
-            cy.contains('a', columnType).click()
-            cy.get('.multiselect__tags').click()
-            cy.get('.multiselect__tags').type(`${nameTree} {enter}`)
-        }
-
-        if(columnType == "Tag Engagement"){
-            cy.contains('a', columnType).click()
-        }
-
-        if(columnType == "Tree Results by Tag"){
-            cy.contains('a', columnType).click()
-        }
-
+    if (columnType == "Tag Engagement") {
+      cy.contains("a", columnType).click();
     }
-    
+
+    if (columnType == "Tree Results by Tag") {
+      cy.contains("a", columnType).click();
+    }
+  }
 }
 
-export default InteractionReport_Objects
+export default InteractionReport_Objects;
